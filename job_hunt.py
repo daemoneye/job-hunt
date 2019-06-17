a9d0d0462713bb0202a056ac8dd3332c41fc45e6
Keane Wolter <daemoneye@localhost.localdomain>
2019-06-17 12:39:39 -0400

    Basic comment outline for program

---
+++ job_hunt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# class for each application
+
+# argparse
+# Argument for non-standard config path
+# Arguments for overrides:
+#  - content location
+#  - timeout for no response
+#  - color scheme
+#     - waiting for response
+#     - Expired by timeout
+#     - Process completed
+#     - Interviewing
+
+# Load json file, grabbed from config
+
+# Add new job
+
+# Update job information
+
+# display data
